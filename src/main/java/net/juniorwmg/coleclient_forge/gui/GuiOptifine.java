42a7df4be2880717091ac63c5a1135def001cc44
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 20:28:10 +0200

    finished anti-optifine

--- src/main/java/net/juniorwmg/coleclient_forge/gui/GuiOptifine.java
+++ src/main/java/net/juniorwmg/coleclient_forge/gui/GuiOptifine.java
@@ -1,17 +1,19 @@
 package net.juniorwmg.coleclient_forge.gui;
 
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 
 public class GuiOptifine extends GuiScreen {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
         this.drawDefaultBackground();
-        this.drawCenteredString(this.fontRenderer, "DO NOT USE OPTIFINE!" +
-                "OptiFine is not supported and may break functionality and compatibility." +
-                "Only proceed if you know what you're doing!" +
-                "You will not receive ANY support." +
-                "Proceed by pressing [ESC].", this.width / 2, this.height / 2, 0xFFFFFF);
+        this.drawCenteredString(this.fontRenderer, "\u00A7c\u00A7lDO NOT USE OPTIFINE!", this.width / 2, this.height / 7, 0xFFFFFF);
+        this.drawCenteredString(this.fontRenderer, "OptiFine is not supported and may break functionality and compatibility.", this.width / 2, this.height / 5, 0xFFFFFF);
+        this.drawCenteredString(this.fontRenderer, "Only proceed if you know what you're doing! You will NOT receive ANY support.", this.width / 2, this.height / 4, 0xFFFFFF);
+        this.drawCenteredString(this.fontRenderer, "Proceed by pressing [ESC].", this.width / 2, this.height / 2, 0xFFFFFF);
+
     }
 }
 
