27d77994225187e228188edea59643b2416b32a3
BuildTools <unconfigured@null.spigotmc.org>
2023-08-13 23:18:42 +0200

    Added config file + first config element

--- src/main/java/net/juniorwmg/coleclient_forge/Main.java
+++ src/main/java/net/juniorwmg/coleclient_forge/Main.java
@@ -2,16 +2,38 @@ package net.juniorwmg.coleclient_forge;
 
 import net.juniorwmg.coleclient_forge.event.CustomEventHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Main.MOD_ID, name = "ColeClient")
+import java.io.File;
+
+@Mod(modid = Main.MOD_ID, name = "ColeClient", clientSideOnly = true)
 public class Main {
     public static final String MOD_ID = "coleclient_forge";
+    private static Configuration configuration;
+    private static boolean userCapes;
 
     @Mod.EventHandler
     public void onPreInit(FMLPreInitializationEvent event) {
-        System.out.println("AMERICANS, LISTEN! YOUR CHOCOLATE IS REAAAAAALLY BAD!");
+
+        try {
+            configuration = new Configuration(new File(event.getModConfigurationDirectory(), "coleclient.cfg"));
+            configuration.load();
+            userCapes = configuration.get("cosmetic", "User capes", true, "Give users their capes from OptiFine and MinecraftCapes (if available)").getBoolean();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (configuration.hasChanged())
+                configuration.save();
+        }
+
         MinecraftForge.EVENT_BUS.register(new CustomEventHandler());
+        System.out.println("AMERICANS, LISTEN! YOUR CHOCOLATE IS REAAAAAALLY BAD!");
+    }
+
+    public static boolean isUserCapes() {
+        return userCapes;
     }
 }
